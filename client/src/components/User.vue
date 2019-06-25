5799dff789b213db204ce1d1709a000ce4afdaa6
yujiariyasu <yuji.ariyasu@gmail.com>
2019-06-25 19:37:31 +0900

    add user/show

---
+++ client/src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ userName }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: 'HelloWorld',
+  data: function() {
+    return {
+      userName: String
+    }
+  },
+  methods: {
+    clear () {
+      this.userName = ''
+    }
+  },
+  created () {
+    axios.get('https://7vr72spqg7.execute-api.ap-northeast-1.amazonaws.com/Prod/hello/yujiariyasu?age=3').then(res => {
+      this.userName = this.$route.params.username
+    }).catch(err => {
+      console.error(err)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ client/src/views/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="user">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <User/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import User from '@/components/User.vue'
+
+export default {
+  name: 'user',
+  components: {
+    User
+  }
+}
+</script>
